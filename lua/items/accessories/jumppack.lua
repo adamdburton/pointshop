18b45ae0fa0d1ac868f0a54d15c71d97a27f127d
Adam Burton <adam@burt0n.net>
2013-01-20 10:14:08 +0000

    Added Jump Pack!

---
+++ lua/items/accessories/jumppack.lua
@@ -0,0 +1,25 @@
+ITEM.Name = 'Jump Pack'
+ITEM.Price = 1000
+ITEM.Model = 'models/xqm/jetengine.mdl'
+ITEM.Bone = 'ValveBiped.Bip01_Spine2'
+
+function ITEM:OnEquip(ply, modifications)
+	ply:PS_AddClientsideModel(self.ID)
+end
+
+function ITEM:OnHolster(ply)
+	ply:PS_RemoveClientsideModel(self.ID)
+end
+
+function ITEM:ModifyClientsideModel(ply, model, pos, ang)
+	model:SetModelScale(0.5, 0)
+	pos = pos + (ang:Right() * 7) + (ang:Forward() * 6)
+	
+	return model, pos, ang
+end
+
+function ITEM:Think(ply, modifications)
+	if ply:KeyDown(IN_JUMP) then
+		ply:SetVelocity(ply:GetUp() * 6)
+	end
+end
