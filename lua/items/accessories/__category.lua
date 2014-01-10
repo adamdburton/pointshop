ec27bece23a911b90ad4648a65669334b787227a
Adam Burton <adam@burt0n.net>
2014-01-10 12:16:59 +0000

    Remove example

--- lua/items/accessories/__category.lua
+++ lua/items/accessories/__category.lua
@@ -1,13 +1,2 @@
 CATEGORY.Name = 'Accessories'
 CATEGORY.Icon = 'add'
-
-function CATEGORY:ModifyTab(tab)
-	local button = vgui.Create('DButton', tab)
-	
-	button:SetText('Click me!')
-	button:Dock(BOTTOM)
-	
-	button.DoClick = function()
-		Derma_Message('You clicked the button!', 'CLICKED', 'OK')
-	end
-end
