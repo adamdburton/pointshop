004e8ffb07ce92fb36cd36b290854ff757c436d0
Alex Grist-Hucker <alexgrist@users.noreply.github.com>
2014-12-12 16:37:23 +0000

    Added ServerGuard rank support.

--- lua/pointshop/sh_player_extension.lua
+++ lua/pointshop/sh_player_extension.lua
@@ -4,6 +4,7 @@ local Player = FindMetaTable('Player')
 -- This had to be done..
 function Player:PS_GetUsergroup()
 	if ( self.EV_GetRank ) then return self:EV_GetRank() end
+	if ( serverguard ) then return serverguard.player:GetRank(self) end
 	-- add for each conflicting admin mod.
 
 	return self:GetNWString('UserGroup')
