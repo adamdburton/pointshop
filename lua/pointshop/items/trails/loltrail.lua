4ca0a094d0b4b65e23deeaaabf1906fa0400c3f9
Sahkay <8295576+Sahkay@users.noreply.github.com>
2018-08-15 12:48:18 +0930

    Update loltrail.lua

--- lua/pointshop/items/trails/loltrail.lua
+++ lua/pointshop/items/trails/loltrail.lua
@@ -3,7 +3,9 @@ ITEM.Price = 150
 ITEM.Material = 'trails/lol.vmt'
 
 function ITEM:OnEquip(ply, modifications)
-	ply.LolTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	if !IsValid(ply.LolTrail) then
+		ply.LolTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	end
 end
 
 function ITEM:OnHolster(ply)
