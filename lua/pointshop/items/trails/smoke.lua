06270a8bbe35f6a6b2bbc208c2a077e347a03956
Sahkay <8295576+Sahkay@users.noreply.github.com>
2018-08-15 12:49:41 +0930

    Update smoke.lua

--- lua/pointshop/items/trails/smoke.lua
+++ lua/pointshop/items/trails/smoke.lua
@@ -3,7 +3,9 @@ ITEM.Price = 150
 ITEM.Material = 'trails/smoke.vmt'
 
 function ITEM:OnEquip(ply, modifications)
-	ply.SmokeTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	if !IsValid(ply.SmokeTrail) then
+		ply.SmokeTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	end
 end
 
 function ITEM:OnHolster(ply)
