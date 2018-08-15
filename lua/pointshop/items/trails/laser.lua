113c41ffc8ab9a9f06f23ce3ddde0243748af0fc
Sahkay <8295576+Sahkay@users.noreply.github.com>
2018-08-15 12:47:47 +0930

    Update laser.lua

--- lua/pointshop/items/trails/laser.lua
+++ lua/pointshop/items/trails/laser.lua
@@ -3,7 +3,9 @@ ITEM.Price = 150
 ITEM.Material = 'trails/laser.vmt'
 
 function ITEM:OnEquip(ply, modifications)
-	ply.LaserTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	if !IsValid(ply.LaserTrail) then
+		ply.LaserTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	end
 end
 
 function ITEM:OnHolster(ply)
