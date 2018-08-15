2f71f4148666a26be765f3295efcf51c66da28b5
Sahkay <8295576+Sahkay@users.noreply.github.com>
2018-08-15 12:49:18 +0930

    Update plasmatrail.lua

--- lua/pointshop/items/trails/plasmatrail.lua
+++ lua/pointshop/items/trails/plasmatrail.lua
@@ -3,7 +3,9 @@ ITEM.Price = 150
 ITEM.Material = 'trails/plasma.vmt'
 
 function ITEM:OnEquip(ply, modifications)
-	ply.PlasmaTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	if !IsValid(ply.PlasmaTrail) then
+		ply.PlasmaTrail = util.SpriteTrail(ply, 0, modifications.color, false, 15, 1, 4, 0.125, self.Material)
+	end
 end
 
 function ITEM:OnHolster(ply)
